7aa80f57443e62fb04bd30cefb77e406ac305721
Blaine Schmeisser <blainesch@gmail.com>
2016-08-14 23:41:27 -0700

    Time the cucumber tests.

--- features/support/after_hooks.js
+++ features/support/after_hooks.js
@@ -1,9 +1,11 @@
 const jetpack = require('fs-jetpack')
 const path = require('path')
+const chalk = require('chalk')
 const { git } = require('../../app/lib/git')
 
 module.exports = function () {
   this.Before(function (scenario) {
+    this.start = new Date()
     const homeDir = path.join(__dirname, '../../test/fixtures/home')
     const calcPlugin = path.join(homeDir, '.zazu', 'plugins', 'tinytacoteam', 'zazu-calculator')
     const configFile = path.join(__dirname, '..', '..', 'test', 'fixtures', 'home', '.zazurc.js')
@@ -14,6 +16,9 @@ module.exports = function () {
   })
 
   this.After(function (scenario) {
+    const duration = new Date() - this.start
+    const color = duration > 3500 ? 'red' : (duration > 2000 ? 'yellow' : 'blue')
+    console.log('    Duration: ', chalk[color](duration + 'ms'))
     return this.close()
   })
 }
